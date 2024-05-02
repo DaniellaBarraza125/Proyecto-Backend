95eefb228647e4f6ee375d1cadffe585b561a164
[DaniellaBarraza125] <daniellabarraza125@gmail.com>
2024-05-02 20:04:04 +0200

    order_products y authentification

--- controllers/OrderController.js
+++ controllers/OrderController.js
@@ -1,16 +1,18 @@
 const { NOW } = require("sequelize");
-const { Order } = require("../models/index.js");
+const { Order, Product, OrderCategory } = require("../models/index.js");
 
 const OrderController = {
     async create(req, res) {
         try {
             const deliveryDate = new Date();
             deliveryDate.setDate(deliveryDate.getDate() + 4);
+
             const order = await Order.create({
                 ...req.body,
                 delivery: deliveryDate,
                 status: "placed",
             });
+            order.addProduct(req.body.ProductId);
 
             res.status(201).send({
                 message: "Pedido creado con éxito",
