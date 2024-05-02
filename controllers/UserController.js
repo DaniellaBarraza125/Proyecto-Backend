f03993f55fc7e201aae679c155cbf4baa7ada8cf
[DaniellaBarraza125] <daniellabarraza125@gmail.com>
2024-05-02 18:40:54 +0200

    user.get

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,5 +1,8 @@
-const { User } = require("../models/index.js");
+const { User, Token } = require("../models/index.js");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const user = require("../models/user.js");
+const { jwt_secret } = require("../config/config.json")["development"];
 
 const UserController = {
     async create(req, res) {
@@ -11,6 +14,35 @@ const UserController = {
                 role: "user",
             });
             res.status(201).send({ message: "Usuario creado con éxito", user });
+        } catch (error) {
+            console.error(error);
+            res.status(500).send(error);
+        }
+    },
+    async getAll(req, res) {
+        try {
+            const users = await User.findAll();
+            res.send({ msg: "Todos los usuarios", users });
+        } catch (error) {
+            console.error(error);
+            res.status(500).send(error);
+        }
+    },
+    async getByName(req, res) {
+        try {
+            const user = await User.findOne({
+                where: {
+                    name: req.params.name,
+                },
+            });
+
+            if (!user) {
+                return res
+                    .status(404)
+                    .send({ message: "Usuario no encontrado" });
+            }
+
+            res.send({ message: "Usuario encontrado", user });
         } catch (error) {
             console.error(error);
             res.status(500).send(error);
@@ -38,8 +70,9 @@ const UserController = {
                     .status(400)
                     .send({ message: "Usuario o contraseña incorrectos" });
             }
-
-            res.send({ messsage: `${user.name}, logeado` });
+            const token = jwt.sign({ id: user.id }, jwt_secret);
+            Token.create({ token, UserId: user.id });
+            res.send({ message: "Bienvenid@ " + user.name, user, token });
         } catch (error) {
             console.error(error);
             res.status(500).send(error);
--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,8 @@ const UserController = require("../controllers/UserController");
 const router = express.Router();
 
 router.post("/", UserController.create);
+router.get("/", UserController.getAll);
+router.get("/name/:name", UserController.getByName);
 router.post("/login", UserController.login);
 
 module.exports = router;
